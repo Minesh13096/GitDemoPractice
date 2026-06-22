ccfc8ca62be8837d67e7f2c5752d353f0279af1b
Minesh13096 <minesh13096@gmail.com>
2026-06-22 15:20:25 +0530

    Clean up print statements in add
    Removed unnecessary print statements and cleaned up code.

--- add.py
+++ add.py
@@ -2,11 +2,3 @@ a = 10
 b = 20
 c = a + b
 print("The sum of a and b is:", c)
-
-<<<<<<< HEAD
-print("This is a simple addition program. vuvhxvuhvx")
-=======
-print("This is a simple addition program.") 
-
-print()print("You can change the values of a and b to see different results.")
->>>>>>> f3852ffe8a8941d5f6db63bc2265cb10bc42b160
